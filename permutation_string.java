96521baf95af99186bd6aa7cddef8cdf3b72a2b6
Shivam Rawat <ajsivamaj@gmail.com>
2022-03-10 20:44:49 +0530

    test case changed

--- permutation_string.java
+++ permutation_string.java
@@ -1,22 +1,31 @@
+import java.util.HashSet;
+
 public class permutation_string {
-    public static void print_permu(String str,String permu) {
+    
+
+    public static void print_permu(String str,String permu,HashSet<String>A) {
 
         if(str.length()==0)
         {
-            System.out.println(permu);
+            A.add(permu);
             return;
         }
         for(int i=0;i<str.length();i++)
         {
             char curr=str.charAt(i);
             String newStr=str.substring(0,i)+str.substring(i+1);
-            print_permu(newStr, permu+curr);
+            print_permu(newStr, permu+curr,A);
 
         }
     }
     public static void main(String[] args) {
-        String st="abc";
-        print_permu(st,"");
+        HashSet<String>A=new HashSet<String>();
+        String st="aatt";
+        print_permu(st,"",A);
+        //for(int i:A)
+        
+            System.out.println(A);
+        
     }
     
 }
